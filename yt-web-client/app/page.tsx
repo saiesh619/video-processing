2b3b8cbd9aae017425dde147390aeba372f70fa7
saiesh619 <prabhusaiesh19@gmail.com>
2025-01-25 13:36:04 -0500

    Added firebase and Next Js UI

---
+++ yt-web-client/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (      
+    <div>
+Get started by editing
+app/page.tsx         
+    </div>
+                                               
+
+  );
+  
+}
